921e24f2fb841265fe2c911205b6def6d78342d2
Alan-delete <62469498+Alan-delete@users.noreply.github.com>
2021-06-09 00:04:56 +0800

    Update score_func.py

--- oracle/udf/happy_moment/score_func.py
+++ oracle/udf/happy_moment/score_func.py
@@ -1,147 +1,118 @@
-# refer to furkanc/Yolov3-Face-Recognition
-# I've used InsightFace_Pytorch for face recognition.
 import sys
 import os
 import io
 
-import cv2
-import argparse
-import numpy as np
+import matplotlib
 
 from oracle.udf.base import BaseScoringUDF
 import config
 sys.path.insert(0, os.path.dirname(__file__))
 
-
 import torch
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib.ticker import NullLocator
 from PIL import Image
+
+from utils.parse_config import parse_model_config
+from models import *
 from utils.utils import *
+import cv2
+import numpy as np
 
 from utils.happy_moment_tools import sentiment_analysis 
 from utils.alexnet import KitModel as AlexNet
 from utils.vgg19 import KitModel as VGG19
-from tqdm import tqdm
 
 
+matplotlib.use('agg')
 
 
-class HappyMoment(BaseScoringUDF):
+obj_names = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
+
+class NumberOfCars(BaseScoringUDF):
     def __init__(self):
         super().__init__()
         self.arg_parser.add_argument("--class_thres", type=float, default=0.5)
         self.arg_parser.add_argument("--obj_thres", type=float, default=0)
-        self.arg_parser.add_argument("--obj", type=str, choices=obj_names, default="person")     
+        self.arg_parser.add_argument("--obj", type=str, choices=obj_names, default="person")
         self.model_config = 'config/yolov3.cfg'
         self.weights = 'weights/yolov3.weights'
-        
-  
-    def initialize(self, opt, gpu=None, prerained_model_name = 'vgg19_finetuned_all'):
+    
+    def initialize(self, opt, gpu=None,sentiment_model_name = 'vgg19_finetuned_all'):
         self.opt = opt
-        
+        self.obj = obj_names.index(opt.obj)
         self.device = config.device
         if gpu is not None:
             self.device = torch.device(f"cuda:{gpu}")
-            
-        print("Loading YOLOv3 weights...")
-        self.net = cv2.dnn.readNetFromDarknet(self.model_config, self.weights)
-        print("Done...")
+        self.model = Darknet(parse_model_config(self.model_config)).to(self.device)
+        self.model.load_darknet_weights(self.weights)
+        self.model.eval()
         
         print("Loading sentiment analysis weights...")
-        self.model = AlexNet if 'hybrid' in prerained_model_name else VGG19
-        self.model = self.model('weights/{}.pth'.format(prerained_model_name)).to('cuda')
-        self.model.eval()
+        self.sentiment_model = AlexNet if 'hybrid' in sentiment_model_name else VGG19
+        self.sentiment_model = self.model('weights/{}.pth'.format(sentiment_model_name)).to('cuda')
+        self.sentiment_model.eval()
         print("Done...")
-        
-        
+    
     def get_img_size(self):
         return (416, 416)
-      
+    
     def get_scores(self, imgs, visualize=False):
-        
-        ln = self.net.getLayerNames()
-        ln = [ln[i[0] - 1] for i in self.net.getUnconnectedOutLayers()]
-    
-        (W, H) = (None, None)
-    
+        assert (imgs.shape[1], imgs.shape[2]) == self.get_img_size()
+        model_imgs = torch.from_numpy(imgs).float().to(self.device)
+        model_imgs = model_imgs.permute(0, 3, 1, 2).contiguous().div(255)
+
+        with torch.no_grad():
+            detections = self.model(model_imgs)
+            detections = non_max_suppression(detections, 0.1, 0.45)
+
+
         scores = [0 for i in range(len(imgs))]
+        visual_imgs = []
         record = {}
         crop_idx = 0
         frame_idx = 0
         cropped_imgs = []
-        visual_imgs = []
-
-       
-        for frame in tqdm(imgs):
-            if W is None or H is None:
-                (H, W) = frame.shape[:2]
         
-            # create a blob from frame and forward it to YOLO.
-            # it will give us the bounding boxes and probabilities
-            blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), (0, 0, 0), swapRB=True, crop=False)
-            self.net.setInput(blob)
-            outputs = self.net.forward(ln)
- 
-       
-            for output in outputs:
-
-                for detected in output:
-
-                    # get class ID and probability.
-                    probability = detected[5:]
-                    class_ID = np.argmax(probability)
-                    confidence = probability[class_ID]
-
-                    # check if probability higher than confidence, class_ID 0 correspond to "person"
-                    if confidence > self.opt.class_thres and class_ID==0:
-                      
-                        # scaling the bounding boxes respect to frame size
-                        # YOLO returns the bounding box parameters in the order of :
-                        # center X , center Y, width , height
-                        box = detected[0:4] * np.array([W, H, W, H])
-                        (center_x, center_y, width, height) = box.astype("int")
-
-                        # find top and left corner of bounding box
+        for i, boxes in enumerate(detections):
+            if boxes is None:
+                if visualize:
+                    visual_imgs.append(imgs[i])
+            else:
+                relavant_boxes = [box for box in boxes if int(box[-1]) == self.obj and float(box[4]) >= self.opt.class_thres and float(box[5]) >= self.opt.obj_thres]
+                
+                visual_img = np.copy(imgs[i])
+                img = np.copy(imgs[i])
+                visual_img = cv2.resize(visual_img, (739, 416))
+                img = cv2.resize(img, (739, 416))
+                
+                for x1, y1, x2, y2, conf, cls_conf, cls_pred in relavant_boxes:
+                    x1 = int(x1.item() / 416 * 739)
+                    x2 = int(x2.item() / 416 * 739)
+                    y1 = int(y1.item())
+                    y2 = int(y2.item())
+                                           
+                    cropped_imgs.append(img[y1:y2, x1:x2])                
+                    record[crop_idx]= frame_idx
+                    crop_idx += 1
                     
-                        x1 = int(center_x - (width / 2))
-
-                        y1 = int(center_y - (height / 2))
+                    cv2.rectangle(visual_img, (x1, y1), (x2, y2), (255, 0, 0), 2)
                     
-                        x2 = x1 + width
-                   
-                        y2 = y1 + height
-                    
-                        # select and crop the box area 
-                        image = frame[y1:y2, x1:x2]
-                        cropped_imgs.append(image)
-                    
-                        record[crop_idx]= frame_idx
-                        
-                        # draw rectangle in the picture 
-                        # Here may results in overflow 
-                        # if visualize:
-                            #visual_img = np.copy(frame)
-                            #cv2.rectangle(visual_img, (x1, y1), (x2, y2), (0, 255, 0), 2)   
-                            #visual_imgs.append(visual_img)
-                            # add to list
-                    
-                    
-                        #boxes.append([x, y, int(width), int(height)])
-                        #confs.append(float(confidence))
-                        crop_idx += 1
-
-            # applying non-maxima suppression
-            #idxs = cv2.dnn.NMSBoxes(boxes, confs, args.confidence, args.nms)            
-    
-            frame_idx += 1 
-
+                if visualize:
+                    visual_imgs.append(Image.fromarray(visual_img))
+            frame_idx += 1
+        
         crop_scores = sentiment_analysis(cropped_imgs, self.model)
-    
+        
         for i, crop_score in enumerate(crop_scores):
             scores[record[i]] += crop_score
-
+            
         if visualize:
             return scores, visual_imgs
         else:
             return scores
-
-       
+            
+            
+            
+            
